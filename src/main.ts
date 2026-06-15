7e92c6bac2eaae4d242ce4e02fc0ee8c9c14c33d
Eazfora <agunzprogrammer@gmail.com>
2026-06-16 00:22:01 +0700

    nambah url

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+    origin: [
+      'http://localhost:5173',
+      'http://127.0.0.1:5173',
+      'https://frontend-client-vercel.vercel.app',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
